a5b2e83f646d02645bb2a673589e2c4ca4666692
MoAziz123 <46410153+MoAziz123@users.noreply.github.com>
2019-03-27 23:47:31 +0000

    Update client1.py

--- client1.py
+++ client1.py
@@ -9,9 +9,9 @@ port = int(sys.argv[2])
 f = open("DBfile.txt", "r") #ensure that r is in string format, or else won't compile cdoe
 list = []
 list2 = []
-y = open("DBfilesizez.txt", "r")
+y = open("DBfilesizez.txt", "r") #open file to send
 for i in f.readlines():
-    list.append(i)
+    list.append(i) # append it to the list
 
 message = list
 
@@ -26,7 +26,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
     print("Connected to", host, port)
     for i in message:
-        s.sendall(i.encode())
+        s.sendall(i.encode()) #encode to binary
     for i in message2:
         s.sendall(i.encode())
     s.sendall(b"End of Data")
