66c127382d797899bd57e30cdc844d721b142468
MoAziz123 <46410153+MoAziz123@users.noreply.github.com>
2019-10-12 19:15:55 +0100

    Add files via upload

---
+++ DebugClient/DebugClient/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DebugClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(94, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(302, 277);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "List:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 463);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DebugClient/DebugClient/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using DebugLog;
+namespace DebugClient
+{
+    public partial class Form1 : Form
+    {
+        public List<string> list = new List<string>();
+        public Debug bug = new Debug();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+
+        public void textBox1_TextChanged(object sender, EventArgs e) //used for search
+        {
+
+            string text = textBox1.Text;
+            string path = string.Format("debug.txt");
+            listBox1.Items.Clear(); //clearing listbox means that it will only give matching queries
+                                    //because without clearing it will keep adding
+                                    
+            foreach (string i in list) //it then only adds matching queries
+            {
+
+                if (i.Contains(text))
+                {
+                    listBox1.Items.Add(i);
+                }
+                
+            }
+            
+        }
+
+
+
+
+    
+
+        public void listBox1_Click(object sender, EventArgs e) //on click i want a bigger update of everything
+        {
+            string text2 = (string)listBox1.SelectedItem;
+            var exception = text2.Substring(0, text2.IndexOf(":"));
+            var description = text2.Substring(text2.IndexOf(":") +1, text2.Length - exception.Length -1);
+            //requires -exception.Length because it alters the original string
+            //also because it's a length parameter, rather than a second index parameter
+            //because of that, - len(exception) - makes it so that the length of the substring matches the end of the string
+            
+
+
+            MessageBox.Show(string.Format("Exception: {0}\n\n Description: {1}", exception, description));
+        }
+
+        public void Form1_Load(object sender, EventArgs e) //on load, give me all the bugs that i've encountered thus far
+        {
+            using (StreamReader r = new StreamReader("debug.txt"))
+            {
+                while (r.Peek() >= 0) //another way to get all lines from  a file, other than != null
+                    //use (line = reader.ReadLine()) != null - to get best results, because it won't repeat its call later on (thus skipping lines)
+                {
+                    list.Add(r.ReadLine());
+                }
+                
+                
+
+            }
+            foreach(string i in list) //so i can access it again for search queries
+            {
+                listBox1.Items.Add(i); 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            using(StreamWriter r = new StreamWriter("debug.txt"))
+            {
+                r.Write(""); //clears document
+            }
+        }
+    }
+}
